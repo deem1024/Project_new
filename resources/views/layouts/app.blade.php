a5cbe9a8670a445b44ee17c288a5fcaee61eefdf
Nadeem Kareme <deemk28@gmail.com>
2022-01-21 15:04:13 +0700

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Look Sao Salad') }}</title>
+    <title>{{('Look Sao Salad') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
